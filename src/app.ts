f4ceff5307f2a259142b0b229fa5674ae4bb976e
Aydan Pirani <aydanpirani@gmail.com>
2024-03-26 10:19:03 -0500

    Added app-level middleware for logging

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,22 @@ import express from "express";
 import { Config } from "./config";
 import { connectToDatabase } from "./utilities";
 import { StatusCodes } from "http-status-codes";
-
+import morgan from  "morgan";
 import authRouter from "./services/auth/auth-router";
 
 const app = express();
 
+// to prevent server-side caching/returning status code 200
+// (we can remove this later)
+app.disable('etag');
+
+// To display the logs every time
+app.use("/", morgan("dev"));
+
 app.use("/auth", authRouter);
 
 app.get("/status", (_, res) => {
+    console.log(StatusCodes.OK)
     return res.status(StatusCodes.OK).send("API is alive!");
 });
 
