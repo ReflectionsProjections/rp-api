df43776b5e0e9a127b98dd4187dfc61ca32a6c63
Aydan Pirani <aydanpirani@gmail.com>
2024-04-14 17:37:31 -0500

    add $

--- src/services/subscription/subscription-router.ts
+++ src/services/subscription/subscription-router.ts
@@ -14,7 +14,7 @@ subscriptionRouter.post("/", async (req, res, next) => {
         // Upsert the user info into the corresponding Subscription collection
         await Database.SUBSCRIPTIONS.findOneAndUpdate(
             { mailingList: subscriptionData.mailingList },
-            { addToSet: { subscriptions: subscriptionData.email } },
+            { $addToSet: { subscriptions: subscriptionData.email } },
             { upsert: true, new: true }
         );
         return res.status(StatusCodes.CREATED).json(subscriptionData);
