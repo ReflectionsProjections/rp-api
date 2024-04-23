676acd0a8b39a61eefbf204e819faf344c834f2b
Alex Yang <32620988+DatProJack@users.noreply.github.com>
2024-04-23 17:58:45 -0500

    formatting

--- src/middleware/role-checker.ts
+++ src/middleware/role-checker.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response, Router } from "express";
+import { NextFunction, Request, Response } from "express";
 import { JwtPayloadValidator, Role } from "../services/auth/auth-models";
 import { z } from "zod";
 import jsonwebtoken from "jsonwebtoken";
