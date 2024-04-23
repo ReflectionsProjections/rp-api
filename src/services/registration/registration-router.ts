0ee1013c72054f3e84b0ce856dbd778c0320a0aa
Jacob Chang <jacobchang124@gmail.com>
2024-04-23 18:25:59 -0500

    registration route update

--- src/services/registration/registration-router.ts
+++ src/services/registration/registration-router.ts
@@ -7,75 +7,86 @@ import {
 import { Database } from "../../database";
 import RoleChecker from "../../middleware/role-checker";
 import { Role } from "../auth/auth-models";
+import { RoleSchema, RoleValidator } from "../auth/auth-schema";
 
 const registrationRouter = Router();
 
 // A database upsert operation to save registration mid-progress
-registrationRouter.post(
-    "/save",
-    RoleChecker(["USER"]),
-    async (req, res, next) => {
-        try {
-            const registrationData = RegistrationValidator.parse(req.body);
+registrationRouter.post("/save", RoleChecker([]), async (req, res, next) => {
+    try {
+        const registrationData = RegistrationValidator.parse(req.body);
 
-            await Database.REGISTRATION.findOneAndUpdate(
-                { email: registrationData.email }, // only required one
-                {
-                    ...registrationData,
-                    complete: false,
-                },
-                { upsert: true, new: true, setDefaultsOnInsert: true }
-            );
+        // Check if user already submitted registration before
+        const existingRegistration = await Database.REGISTRATION.findOne({
+            userId: registrationData.userId,
+            hasSubmitted: true,
+        });
 
-            res.status(StatusCodes.OK).json(registrationData);
-        } catch (error) {
-            next(error);
+        if (existingRegistration) {
+            return res.status(StatusCodes.CONFLICT).json({
+                error: "AlreadySubmitted",
+            });
         }
+
+        await Database.REGISTRATION.findOneAndUpdate(
+            { userId: res.locals.payload.userId },
+            {
+                ...registrationData,
+                hasSubmitted: false,
+            },
+            { upsert: true, new: true, setDefaultsOnInsert: true }
+        );
+
+        return res.status(StatusCodes.OK).json(registrationData);
+    } catch (error) {
+        next(error);
     }
-);
+});
 
-registrationRouter.post(
-    "/submit",
-    RoleChecker(["USER"]),
-    async (req, res, next) => {
-        try {
-            const registrationData = RegistrationValidator.parse(req.body);
+registrationRouter.post("/submit", RoleChecker([]), async (req, res, next) => {
+    try {
+        const registrationData = RegistrationValidator.parse(req.body);
 
-            await Database.REGISTRATION.findOneAndUpdate(
-                { email: registrationData.email }, // only required one
-                {
-                    ...registrationData,
-                    complete: true,
+        await Database.REGISTRATION.findOneAndUpdate(
+            { userId: res.locals.payload.userId },
+            {
+                ...registrationData,
+                hasSubmitted: true,
+            },
+            { upsert: true, new: true, setDefaultsOnInsert: true }
+        );
+
+        await Database.ROLES.findOneAndUpdate(
+            { userId: res.locals.payload.userId },
+            {
+                $addToSet: {
+                    roles: Role.Values.USER,
                 },
-                { upsert: true, new: true, setDefaultsOnInsert: true }
-            );
+            },
+            { upsert: true }
+        );
 
-            res.status(StatusCodes.OK).json(registrationData);
-        } catch (error) {
-            next(error);
-        }
+        return res.status(StatusCodes.OK).json(registrationData);
+    } catch (error) {
+        next(error);
     }
-);
+});
 
 // Retrieve registration fields both to repopulate registration info for a user
-registrationRouter.get(
-    "/get/:email",
-    RoleChecker(["USER"]),
-    async (req, res, next) => {
-        try {
-            const email = req.params.email;
+registrationRouter.get("/get", RoleChecker([]), async (req, res, next) => {
+    try {
+        const registration = await Database.REGISTRATION.findOne({
+            userId: res.locals.payload.userId,
+        });
 
-            const registration = await Database.REGISTRATION.findOne({ email });
-
-            if (!registration) {
-                return { error: "DoesNotExist" };
-            }
-
-            res.status(StatusCodes.OK).json({ registration });
-        } catch (error) {
-            next(error);
+        if (!registration) {
+            return { error: "DoesNotExist" };
         }
+
+        return res.status(StatusCodes.OK).json({ registration });
+    } catch (error) {
+        next(error);
     }
-);
+});
 
 export default registrationRouter;
--- src/services/registration/registration-schema.ts
+++ src/services/registration/registration-schema.ts
@@ -3,22 +3,27 @@ import { z } from "zod";
 
 // Zod schema for registration
 const RegistrationValidator = z.object({
+    userId: z
+        .string()
+        .regex(/user[0-9]*/)
+        .optional(),
     name: z.string().optional(),
     email: z.string().email(),
     events: z.array(z.string()).optional(),
     dietary_restrictions: z.array(z.string()).optional(),
     points: z.number().min(0).default(0).optional(),
-    complete: z.boolean().default(false),
+    hasSubmitted: z.boolean().default(false),
 });
 
 // Mongoose schema for registration
 const RegistrationSchema = new mongoose.Schema({
+    userId: { type: String, required: true, unique: true },
     name: { type: String },
     email: { type: String, required: true, unique: true },
     events: [{ type: mongoose.Schema.Types.ObjectId, ref: "Event" }],
     dietary_restrictions: [{ type: String }],
     points: { type: Number, default: 0 },
-    complete: { type: Boolean, default: false },
+    hasSubmitted: { type: Boolean, default: false },
 });
 
 export { RegistrationSchema, RegistrationValidator };
