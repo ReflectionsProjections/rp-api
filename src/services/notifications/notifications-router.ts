99ef58fb54e9f754b1558e38036522662377397d
divinedab <divyack2@illinois.edu>
2024-04-24 00:21:02 -0500

    more small changes

--- src/services/notifications/notifications-router.ts
+++ src/services/notifications/notifications-router.ts
@@ -12,18 +12,16 @@ notificationsRouter.post(
     "/",
     RoleChecker([Role.enum.USER]),
     async (req, res, next) => {
+        const payload = res.locals.payload;
         try {
             const notificationEnrollmentData = NotificationsValidator.parse(
                 req.body
             );
-
-            const payload = res.locals.payload;
-
             // Upsert the user-device mapping info
             await Database.NOTIFICATIONS.findOneAndUpdate(
                 { userId: payload.userId },
                 { deviceId: notificationEnrollmentData.deviceId },
-                { upsert: true, new: true }
+                { upsert: true }
             );
 
             return res.status(StatusCodes.OK).json(notificationEnrollmentData);
