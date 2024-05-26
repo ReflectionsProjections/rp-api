94ecbea48f512a31b52377b22c8077232338718e
Aydan Pirani <aydanpirani@gmail.com>
2024-05-25 17:48:19 -0700

    Added new endpoint templating

---
+++ src/services/mail/mail-router.ts
@@ -0,0 +1,13 @@
+import draftsSubRouter from "./drafts/drafts-subrouter";
+import listsSubRouter from "./lists/lists-subrouter";
+import templatesSubRouter from "./templates/templates-subrouter";
+
+import { Router } from "express";
+
+const mailRouter = Router();
+
+mailRouter.use("drafts", draftsSubRouter);
+mailRouter.use("lists", listsSubRouter);
+mailRouter.use("templates", templatesSubRouter);
+
+export default mailRouter;
