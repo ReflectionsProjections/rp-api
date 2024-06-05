35cc7855c1df0ff935e48b8a7bd86e86886f3a05
Aydan Pirani <aydanpirani@gmail.com>
2024-06-04 23:06:36 -0700

    Add if-check to start pm2 if and only if app doesn't run

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 cd /home/ubuntu/rp-api
 sudo yarn
+if $(sudo pm2 list | grep -c "RP_API" | grep -v :0);
+then
 sudo pm2 start build/app.js --name RP_API -i 2 --wait-ready --listen-timeout 10000
+fi;
