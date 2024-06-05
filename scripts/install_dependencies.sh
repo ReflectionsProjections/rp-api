e32e9ec19cdb7947ca906c7fc2eb4c5234ff8474
Aydan Pirani <aydanpirani@gmail.com>
2024-06-04 22:47:42 -0700

    Added build folder to script

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/rp-api
 sudo yarn
-sudo pm2 start app.js --name RP_API -i 2 --wait-ready --listen-timeout 10000
+sudo pm2 start build/app.js --name RP_API -i 2 --wait-ready --listen-timeout 10000
