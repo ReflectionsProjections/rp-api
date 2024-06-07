9508bfd84dd58b47b0c5f037c3d6d8ad4c58907d
Aydan Pirani <aydanpirani@gmail.com>
2024-06-06 20:56:35 -0700

    script changes

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 cd /home/ubuntu/rp-api
 sudo yarn
-if (($(sudo pm2 list | grep -c "RP_API" | grep -v :0) == 0));
-then
+
+describe appname > /dev/null
+RUNNING=$?
+
+
+if [[ "${RUNNING}" -ne 0 ]]; then
 sudo pm2 start build/app.js --name RP_API -i 2 --wait-ready --listen-timeout 10000
 fi;
