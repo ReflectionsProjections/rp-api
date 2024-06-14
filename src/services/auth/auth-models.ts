72ac69aaaeb3991efc03a001067aece3ea6228b7
anandani4136 <ra7353@hotmail.com>
2024-06-13 19:31:26 -0500

    Rename "name" to "displayName"

--- src/services/auth/auth-models.ts
+++ src/services/auth/auth-models.ts
@@ -4,6 +4,6 @@ export const Role = z.enum(["USER", "STAFF", "ADMIN", "CORPORATE"]);
 
 export const JwtPayloadValidator = z.object({
     userId: z.string(),
-    name: z.string(),
+    displayName: z.string(),
     roles: Role.array(),
 });
