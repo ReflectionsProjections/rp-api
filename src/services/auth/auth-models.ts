0790066f7186a2cf3777d2704f140da27ac20af3
Ronit Anandani <56076011+anandani4136@users.noreply.github.com>
2024-06-13 19:26:39 -0500

    Update auth-models.ts with name field

--- src/services/auth/auth-models.ts
+++ src/services/auth/auth-models.ts
@@ -4,5 +4,6 @@ export const Role = z.enum(["USER", "STAFF", "ADMIN", "CORPORATE"]);
 
 export const JwtPayloadValidator = z.object({
     userId: z.string(),
+    name: z.string(),
     roles: Role.array(),
 });
