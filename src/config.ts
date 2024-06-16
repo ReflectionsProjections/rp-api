d975107fc0adb331ab800b49da2733d5a5523a66
Divya Koya <90989518+divyack2@users.noreply.github.com>
2024-06-16 17:03:12 -0400

    Update config.ts
    changed mobile redirect

--- src/config.ts
+++ src/config.ts
@@ -84,5 +84,5 @@ export const Config = {
 export const DeviceRedirects: Record<string, string> = {
     web: "http://localhost:5173/",
     dev: "https://api.reflectionsprojections.org/auth/dev/",
-    mobile: "http://localhost:8081/Login",
+    mobile: "reflectionsprojections://Main",
 };
