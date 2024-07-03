3fa406ae49b44c06cc0496bccd8302e6ff494012
Alex Yang <32620988+DatProJack@users.noreply.github.com>
2024-07-02 22:58:22 -0400

    added env values for testing env setup

--- testing/jest.env-setup.ts
+++ testing/jest.env-setup.ts
@@ -1,1 +1,17 @@
 process.env.ENV = "TESTING";
+
+process.env.DATABASE_USERNAME = "urmom";
+process.env.DATABASE_PASSWORD = "alsourmom";
+process.env.DATABASE_HOST = "urmomisthebomb.com";
+
+process.env.OAUTH_GOOGLE_CLIENT_ID = "beepbeepimasheep";
+process.env.OAUTH_GOOGLE_CLIENT_SECRET = "isaidbeepbeepimasheep";
+
+process.env.JWT_SIGNING_SECRET = "supersecretsecret";
+process.env.QR_HASH_SECRET = "raidshadowlegends";
+
+process.env.S3_ACCESS_KEY = "2468";
+process.env.S3_SECRET_KEY = "whodeweappreciate";
+process.env.S3_BUCKET_NAME =
+    "theduckwalkeduptothelemonadestandandhesaidtothemanrunningthestandheygotanygrapesandthenthemangotmadandshottheduck";
+process.env.S3_REGION = "aok";
