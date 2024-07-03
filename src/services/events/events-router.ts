db5f17c227f834c610fe2ee9b06fd5cd044a8923
riyap <riyapatel@gmail.com>
2024-07-02 20:50:18 -0700

    last pr fixes

--- src/services/events/events-router.ts
+++ src/services/events/events-router.ts
@@ -5,60 +5,65 @@ import { Database } from "../../database";
 import { checkInUserToEvent } from "./events-utils";
 import RoleChecker from "../../middleware/role-checker";
 import { Role } from "../auth/auth-models";
-import { isAdmin, isStaff, isUser } from "../auth/auth-utils";
+import { isAdmin, isStaff } from "../auth/auth-utils";
 
 const eventsRouter = Router();
 
 // Get current or next event based on current time
-eventsRouter.get("/currentOrNext", RoleChecker([], true), async (req, res, next) => {
-    const currentTime = new Date();
-    const payload = res.locals.payload;
+eventsRouter.get(
+    "/currentOrNext",
+    RoleChecker([], true),
+    async (req, res, next) => {
+        const currentTime = new Date();
+        const payload = res.locals.payload;
 
-    const isUser = !(isStaff(payload) || isAdmin(payload))
+        const isUser = !(isStaff(payload) || isAdmin(payload));
 
-    try {
-        const event = await Database.EVENTS.findOne({
-            startTime: { $gte: currentTime },
-            isVisible: isUser ? { $eq: true } : {},
-        }).sort({ startTime: 1 });
+        try {
+            const event = await Database.EVENTS.findOne({
+                startTime: { $gte: currentTime },
+                isVisible: isUser ? { $eq: true } : {},
+            }).sort({ startTime: 1 });
 
-        if (event) {
-            return res.status(StatusCodes.OK).json(event);
-        } else {
-            return res
-                .status(StatusCodes.NO_CONTENT)
-                .json({ error: "DoesNotExist" });
+            if (event) {
+                return res.status(StatusCodes.OK).json(event);
+            } else {
+                return res
+                    .status(StatusCodes.NO_CONTENT)
+                    .json({ error: "DoesNotExist" });
+            }
+        } catch (error) {
+            next(error);
         }
-    } catch (error) {
-        next(error);
     }
-});
+);
 
 // Get all events
 eventsRouter.get("/", RoleChecker([], true), async (req, res, next) => {
     const payload = res.locals.payload;
-    
+
     var filterFunction;
 
     try {
         var unfiltered_events = await Database.EVENTS.find();
-        
+
         if (isStaff(payload) || isAdmin(payload)) {
             filterFunction = (x: any) => privateEventValidator.parse(x);
         } else {
-            unfiltered_events = unfiltered_events.filter(x => x.isVisible)
+            unfiltered_events = unfiltered_events.filter((x) => x.isVisible);
             filterFunction = (x: any) => publicEventValidator.parse(x);
         }
 
         const filtered_events = unfiltered_events.map(filterFunction);
-        console.log(filtered_events)
+        console.log(filtered_events);
         return res.status(StatusCodes.OK).json(filtered_events);
     } catch (error) {
         next(error);
     }
 });
 
-eventsRouter.get("/:EVENTID", RoleChecker([], true), async (req, res, next) => { // add RoleChecker here as well
+eventsRouter.get("/:EVENTID", RoleChecker([], true), async (req, res, next) => {
+    // add RoleChecker here as well
     const eventId = req.params.EVENTID;
     const payload = res.locals.payload;
 
@@ -91,66 +96,83 @@ eventsRouter.get("/:EVENTID", RoleChecker([], true), async (req, res, next) => {
     }
 });
 
-eventsRouter.post("/", RoleChecker([Role.Enum.STAFF]), async (req, res, next) => {
-    try {
-        const validatedData = privateEventValidator.parse(req.body);
-        const event = new Database.EVENTS(validatedData);
-        await event.save();
-        return res.sendStatus(StatusCodes.CREATED);
-    } catch (error) {
-        next(error);
-    }
-});
-
-eventsRouter.put("/:EVENTID", RoleChecker([Role.Enum.STAFF], true), async (req, res, next) => {
-    const eventId = req.params.EVENTID;
-    try {
-        const validatedData = privateEventValidator.parse(req.body);
-        const event = await Database.EVENTS.findOneAndUpdate(
-            { eventId: eventId },
-            { $set: validatedData }
-        );
-
-        if (!event) {
-            return res
-                .status(StatusCodes.NOT_FOUND)
-                .json({ error: "DoesNotExist" });
+eventsRouter.post(
+    "/",
+    RoleChecker([Role.Enum.STAFF]),
+    async (req, res, next) => {
+        try {
+            const validatedData = privateEventValidator.parse(req.body);
+            const event = new Database.EVENTS(validatedData);
+            await event.save();
+            return res.sendStatus(StatusCodes.CREATED);
+        } catch (error) {
+            next(error);
         }
-
-        return res.sendStatus(StatusCodes.OK);
-    } catch (error) {
-        next(error);
     }
-});
+);
+
+eventsRouter.put(
+    "/:EVENTID",
+    RoleChecker([Role.Enum.STAFF], true),
+    async (req, res, next) => {
+        const eventId = req.params.EVENTID;
+        try {
+            const validatedData = privateEventValidator.parse(req.body);
+            const event = await Database.EVENTS.findOneAndUpdate(
+                { eventId: eventId },
+                { $set: validatedData }
+            );
+
+            if (!event) {
+                return res
+                    .status(StatusCodes.NOT_FOUND)
+                    .json({ error: "DoesNotExist" });
+            }
+
+            return res.sendStatus(StatusCodes.OK);
+        } catch (error) {
+            next(error);
+        }
+    }
+);
 
 // Delete event
-eventsRouter.delete("/:EVENTID", RoleChecker([Role.Enum.STAFF], true), async (req, res, next) => {
-    const eventId = req.params.EVENTID;
-    try {
-        await Database.EVENTS.findOneAndDelete({ eventId: eventId });
+eventsRouter.delete(
+    "/:EVENTID",
+    RoleChecker([Role.Enum.STAFF], true),
+    async (req, res, next) => {
+        const eventId = req.params.EVENTID;
+        try {
+            await Database.EVENTS.findOneAndDelete({ eventId: eventId });
 
-        return res.sendStatus(StatusCodes.NO_CONTENT);
-    } catch (error) {
-        next(error);
-    }
-});
-
-eventsRouter.post("/check-in", RoleChecker([Role.Enum.STAFF], true), async (req, res, next) => { // add RoleChecker for staff
-    try {
-        const { eventId, userId } = req.body;
-        const result = await checkInUserToEvent(eventId, userId);
-        if (result.success) {
-            return res
-                .status(StatusCodes.OK)
-                .json({ message: "Check-in successful" });
-        } else {
-            return res
-                .status(StatusCodes.NOT_FOUND)
-                .json({ error: result.message });
+            return res.sendStatus(StatusCodes.NO_CONTENT);
+        } catch (error) {
+            next(error);
         }
-    } catch (error) {
-        next(error);
     }
-});
+);
+
+eventsRouter.post(
+    "/check-in",
+    RoleChecker([Role.Enum.STAFF], true),
+    async (req, res, next) => {
+        // add RoleChecker for staff
+        try {
+            const { eventId, userId } = req.body;
+            const result = await checkInUserToEvent(eventId, userId);
+            if (result.success) {
+                return res
+                    .status(StatusCodes.OK)
+                    .json({ message: "Check-in successful" });
+            } else {
+                return res
+                    .status(StatusCodes.NOT_FOUND)
+                    .json({ error: result.message });
+            }
+        } catch (error) {
+            next(error);
+        }
+    }
+);
 
 export default eventsRouter;
