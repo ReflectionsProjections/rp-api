a6706ab452d647e826e822dcd78a88dcbe89087c
Dev Patel <devpatel@Devs-MacBook-Pro-2.local>
2024-07-22 15:36:53 -0500

    idk anymore

--- src/services/sponsor/sponsor-router.ts
+++ src/services/sponsor/sponsor-router.ts
@@ -6,7 +6,7 @@ import { StatusCodes } from "http-status-codes";
 import { sendEmail } from "../ses/ses-utils";
 import jsonwebtoken from "jsonwebtoken";
 import { Config } from "../../config";
-const bcrypt = require("bcrypt");
+import * as bcrypt from 'bcrypt';
 const sponsorRouter = Router();
 
 // Get favorite events for an attendee
@@ -15,7 +15,10 @@ sponsorRouter.get(
     RoleChecker([Role.Enum.CORPORATE]),
     async (req, res, next) => {
         try {
-            const resumeUsers = await Database.REGISTRATION.find({ hasResume: true }, { userId: 1 });
+            const resumeUsers = await Database.REGISTRATION.find(
+                { hasResume: true },
+                { userId: 1 }
+            );
             if (!resumeUsers) {
                 return res
                     .status(StatusCodes.NOT_FOUND)
--- src/services/sponsor/sponsor-schema.ts
+++ src/services/sponsor/sponsor-schema.ts
@@ -3,12 +3,12 @@ import { z } from "zod";
 
 export const SponsorSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
-    hashedVerificationCode: { type: String, required: true }, // Ensure this matches your zod schema
-    expTime: { type: Number, required: true }
+    hashedVerificationCode: { type: String, required: true },
+    expTime: { type: Number, required: true },
 });
 
 export const SponsorValidator = z.object({
     email: z.string().email(),
-    hashedVerificationCode: z.string(), // Ensure this matches your mongoose schema
-    expTime: z.number().int()
+    hashedVerificationCode: z.string(), 
+    expTime: z.number().int(),
 });
