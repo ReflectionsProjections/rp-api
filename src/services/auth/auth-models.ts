5299c8e6d1208e3a97d94ca79aa9ae382477f491
Aydan Pirani <aydanpirani@example.com>
2024-07-27 00:24:35 -0700

    Added PUZZLEBANG role

--- src/services/auth/auth-models.ts
+++ src/services/auth/auth-models.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-export const Role = z.enum(["USER", "STAFF", "ADMIN", "CORPORATE"]);
+export const Role = z.enum(["USER", "STAFF", "ADMIN", "CORPORATE", "PUZZLEBANG"]);
 
 export const JwtPayloadValidator = z.object({
     userId: z.string(),
