bb5225305661ac0c6ee426fc9767974882bbb77b
Jacob Chang <jacobchang124@gmail.com>
2024-08-27 13:42:45 -0500

    update validator

--- src/services/speakers/speakers-validators.ts
+++ src/services/speakers/speakers-validators.ts
@@ -3,7 +3,6 @@ import { v4 as uuidv4 } from "uuid";
 
 // Zod schema for speaker
 export const SpeakerValidator = z.object({
-    speakerId: z.coerce.string().default(() => uuidv4()),
     name: z.string(),
     title: z.string(),
     bio: z.string(),
