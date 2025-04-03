08afbe67d47185c7c50d21f3eba0c3645eaf26ee
Ritam nandi <nandi.ritamnandi@gmail.com>
2025-04-02 21:14:05 -0500

    lint

--- src/services/meetings/meetings-router.ts
+++ src/services/meetings/meetings-router.ts
@@ -13,100 +13,116 @@ const meetingsRouter = Router();
 // PUT endpoints: edit a meeting
 // DELETE endpoints: delete a meeting
 
-// For the endpoints, I should probably add role checking
-// Just not sure if we've set up roles yet, so I'll keep that as a todo
-// @todo add actual role checking
-
 // get all events
-meetingsRouter.get("/", RoleChecker([Role.enum.STAFF, Role.enum.ADMIN], true), async (req, res, next) => {
-    try {
-        const meetings = await Database.MEETINGS.find();
-        const parsedMeetings = meetings.map((meeting) =>
-            meetingView.parse(meeting.toObject())
-        );
-        res.status(StatusCodes.OK).json(parsedMeetings);
-    } catch (error) {
-        next(error);
+meetingsRouter.get(
+    "/",
+    RoleChecker([Role.enum.STAFF, Role.enum.ADMIN], true),
+    async (req, res, next) => {
+        try {
+            const meetings = await Database.MEETINGS.find();
+            const parsedMeetings = meetings.map((meeting) =>
+                meetingView.parse(meeting.toObject())
+            );
+            res.status(StatusCodes.OK).json(parsedMeetings);
+        } catch (error) {
+            next(error);
+        }
     }
-});
+);
 
 // get specific event
-meetingsRouter.get("/:meetingId", RoleChecker([Role.enum.STAFF, Role.enum.ADMIN], true), async (req, res, next) => {
-    try {
-        const { meetingId } = req.params;
-        const meeting = await Database.MEETINGS.findOne({ meetingId });
+meetingsRouter.get(
+    "/:meetingId",
+    RoleChecker([Role.enum.STAFF, Role.enum.ADMIN], true),
+    async (req, res, next) => {
+        try {
+            const { meetingId } = req.params;
+            const meeting = await Database.MEETINGS.findOne({ meetingId });
 
-        if (!meeting) {
-            return res
-                .status(StatusCodes.NOT_FOUND)
-                .json({ message: "Meeting not found" });
+            if (!meeting) {
+                return res
+                    .status(StatusCodes.NOT_FOUND)
+                    .json({ message: "Meeting not found" });
+            }
+
+            const parsedMeeting = meetingView.parse(meeting.toObject());
+            res.status(StatusCodes.OK).json(parsedMeeting);
+        } catch (error) {
+            next(error);
         }
-
-        const parsedMeeting = meetingView.parse(meeting.toObject());
-        res.status(StatusCodes.OK).json(parsedMeeting);
-    } catch (error) {
-        next(error);
     }
-});
+);
 
 // create an event
-meetingsRouter.post("/", RoleChecker([Role.enum.ADMIN], true), async (req, res, next) => {
-    try {
-        const validatedData = meetingValidator.parse(req.body);
-        const newMeeting = new Database.MEETINGS(validatedData);
-        await newMeeting.save();
+meetingsRouter.post(
+    "/",
+    RoleChecker([Role.enum.ADMIN], true),
+    async (req, res, next) => {
+        try {
+            const validatedData = meetingValidator.parse(req.body);
+            const newMeeting = new Database.MEETINGS(validatedData);
+            await newMeeting.save();
 
-        const parsedMeeting = meetingView.parse(newMeeting.toObject());
-        res.status(StatusCodes.CREATED).json(parsedMeeting);
-    } catch (error) {
-        next(error);
+            const parsedMeeting = meetingView.parse(newMeeting.toObject());
+            res.status(StatusCodes.CREATED).json(parsedMeeting);
+        } catch (error) {
+            next(error);
+        }
     }
-});
+);
 
 // edit a meeting, parameter is the ID
-meetingsRouter.put("/:meetingId", RoleChecker([Role.enum.ADMIN], true), async (req, res, next) => {
-    try {
-        const { meetingId } = req.params;
-        const parsedData = meetingValidator.partial().parse(req.body);
+meetingsRouter.put(
+    "/:meetingId",
+    RoleChecker([Role.enum.ADMIN], true),
+    async (req, res, next) => {
+        try {
+            const { meetingId } = req.params;
+            const parsedData = meetingValidator.partial().parse(req.body);
 
-        const updatedMeeting = await Database.MEETINGS.findOneAndUpdate(
-            { meetingId },
-            parsedData,
-            { new: true }
-        );
+            const updatedMeeting = await Database.MEETINGS.findOneAndUpdate(
+                { meetingId },
+                parsedData,
+                { new: true }
+            );
 
-        if (!updatedMeeting) {
-            return res
-                .status(StatusCodes.NOT_FOUND)
-                .json({ message: "Meeting not found" });
+            if (!updatedMeeting) {
+                return res
+                    .status(StatusCodes.NOT_FOUND)
+                    .json({ message: "Meeting not found" });
+            }
+
+            const parsedMeeting = meetingView.parse(updatedMeeting.toObject());
+            res.status(StatusCodes.OK).json(parsedMeeting);
+        } catch (error) {
+            next(error);
         }
-
-        const parsedMeeting = meetingView.parse(updatedMeeting.toObject());
-        res.status(StatusCodes.OK).json(parsedMeeting);
-    } catch (error) {
-        next(error);
     }
-});
+);
 
 // delete a meeting, by meeting ID
 
-meetingsRouter.delete("/:meetingId", RoleChecker([Role.enum.ADMIN], true), async (req, res, next) => {
-    try {
-        const { meetingId } = req.params;
-        const deletedMeeting = await Database.MEETINGS.findOneAndDelete({
-            meetingId,
-        });
+meetingsRouter.delete(
+    "/:meetingId",
+    RoleChecker([Role.enum.ADMIN], true),
+    async (req, res, next) => {
+        try {
+            const { meetingId } = req.params;
+            const deletedMeeting = await Database.MEETINGS.findOneAndDelete({
+                meetingId,
+            });
 
-        if (!deletedMeeting) {
-            return res
-                .status(StatusCodes.NOT_FOUND)
-                .json({ message: "Meeting not found" });
+            if (!deletedMeeting) {
+                return res
+                    .status(StatusCodes.NOT_FOUND)
+                    .json({ message: "Meeting not found" });
+            }
+
+            res.status(StatusCodes.NO_CONTENT).send(); // 204 No Content on successful deletion
+        } catch (error) {
+            next(error);
         }
-
-        res.status(StatusCodes.NO_CONTENT).send(); // 204 No Content on successful deletion
-    } catch (error) {
-        next(error);
     }
-});
+);
 
 export default meetingsRouter;
