6cbcdb35cbf9f39be4cd32c9b5931991d789c37f
anandani4136 <ra7353@hotmail.com>
2025-05-30 00:43:16 -0500

    Remove ANON_KEY

--- testing/jest.env-setup.ts
+++ testing/jest.env-setup.ts
@@ -18,4 +18,3 @@ process.env.S3_REGION = "aok";
 process.env.USERID_ENCRYPTION_KEY = "whatissecurity";
 
 process.env.SUPABASE_URL = "http://kong:8000";
-process.env.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJzZXJ2aWNlX3JvbGUiLAogICAgImlzcyI6ICJzdXBhYmFzZS1kZW1vIiwKICAgICJpYXQiOiAxNjQxNzY5MjAwLAogICAgImV4cCI6IDE3OTk1MzU2MDAKfQ.DaYlNEoUrrEn2Ig7tqibS-PHK5vgusbcbo7X36XVt4Q";
