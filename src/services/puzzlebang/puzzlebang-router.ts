b5af32459c4754322b81ab73295696da28fca6f3
Ritam Nandi <nandi.ritamnandi@gmail.com>
2025-07-11 15:09:47 -0400

    Using .throwOnError() instead of manualy handling errors

--- src/services/puzzlebang/puzzlebang-router.ts
+++ src/services/puzzlebang/puzzlebang-router.ts
@@ -16,21 +16,11 @@ puzzlebangRouter.post(
         );
 
         // Email is in registration, so we need to get the userId from the email and pull from attendee
-        const { data: registrationData, error: registrationError } =
+        const { data: registrationData } =
             await SupabaseDB.REGISTRATIONS.select("user_id")
                 .eq("email", email)
-                .maybeSingle(); // allow 0 rows, which will be gracefully handled
-
-        if (registrationError) {
-            console.error(
-                "Database error fetching registration:",
-                registrationError
-            );
-            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-                message:
-                    "An unexpected error occurred while fetching registration data.",
-            });
-        }
+                .maybeSingle()
+                .throwOnError(); // allow 0 rows, which will be gracefully handled
 
         if (!registrationData) {
             return res.status(StatusCodes.NOT_FOUND).json({
@@ -40,18 +30,12 @@ puzzlebangRouter.post(
 
         const userId = registrationData.user_id;
 
-        const { data: attendeeData, error: attendeeError } =
-            await SupabaseDB.ATTENDEES.select("puzzles_completed, points")
-                .eq("user_id", userId)
-                .maybeSingle(); // allow 0 rows, which will be gracefully handled
-
-        if (attendeeError) {
-            console.error("Database error fetching attendee:", attendeeError);
-            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-                message:
-                    "An unexpected error occurred while fetching attendee data.",
-            });
-        }
+        const { data: attendeeData } = await SupabaseDB.ATTENDEES.select(
+            "puzzles_completed, points"
+        )
+            .eq("user_id", userId)
+            .maybeSingle()
+            .throwOnError(); // allow 0 rows, which will be gracefully handled
 
         if (!attendeeData) {
             return res
