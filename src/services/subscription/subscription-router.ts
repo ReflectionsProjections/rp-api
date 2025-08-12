f9ef5996bdebda9677ad08833778ecfdb0da94a4
Ritam Nandi <nandi.ritamnandi@gmail.com>
2025-08-12 14:29:38 -0700

    Setting up AWS SES

--- src/services/subscription/subscription-router.ts
+++ src/services/subscription/subscription-router.ts
@@ -2,7 +2,10 @@ import { Router } from "express";
 import { StatusCodes } from "http-status-codes";
 import { SubscriptionValidator } from "./subscription-schema";
 import { SupabaseDB } from "../../supabase";
+import { Role } from "../auth/auth-models";
 import cors from "cors";
+import RoleChecker from "../../middleware/role-checker";
+import { SESv2Client, SendEmailCommand } from "@aws-sdk/client-sesv2";
 
 const subscriptionRouter = Router();
 
@@ -47,5 +50,62 @@ subscriptionRouter.post("/", cors(), async (req, res) => {
 
     return res.status(StatusCodes.CREATED).json(subscriptionData);
 });
+
+// Get a list of all subscriptions - envisioning that admins can use this as dropdown to choose who to send emails to
+subscriptionRouter.get(
+    "/",
+    RoleChecker([Role.Enum.ADMIN]),
+    async (req, res) => {
+        const { data: subscriptions } =
+            await SupabaseDB.SUBSCRIPTIONS.select("*").throwOnError();
+
+        return res.status(StatusCodes.OK).json(subscriptions);
+    }
+);
+
+// Send an email to a mailing list
+// API body: {String} mailingList The list to send the email to, {String} subject The subject line of the email, {String} htmlBody The HTML content of the email.
+subscriptionRouter.post(
+    "/send-email",
+    RoleChecker([Role.Enum.ADMIN]),
+    async (req, res) => {
+        const { mailingList, subject, htmlBody } = req.body;
+        const { data: list } = await SupabaseDB.SUBSCRIPTIONS.select(
+            "subscriptions"
+        )
+            .eq("mailingList", mailingList)
+            .single()
+            .throwOnError();
+
+        const sesClient = new SESv2Client({
+            region: process.env.AWS_REGION!,
+            credentials: {
+                accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+            },
+        });
+
+        const sendEmailCommand = new SendEmailCommand({
+            FromEmailAddress: process.env.FROM_EMAIL_ADDRESS ?? "",
+            Destination: {
+                // SES can send to multiple addresses at once
+                // ToAddresses: list.subscriptions,
+                // Let's send to ourselves for now, and bcc everyone else, probably the most pro way to go about it.
+                ToAddresses: [process.env.FROM_EMAIL_ADDRESS ?? ""],
+                BccAddresses: list.subscriptions,
+            },
+            Content: {
+                Simple: {
+                    Subject: { Data: subject },
+                    Body: { Html: { Data: htmlBody } },
+                },
+            },
+        });
+
+        await sesClient.send(sendEmailCommand);
+
+        return res.status(StatusCodes.OK).send({ status: "success" });
+    }
+);
 
 export default subscriptionRouter;
