8203150813f4128b644a3eb7f646cfe89a571c7d
Timothy Gonzalez <105177619+Timothy-Gonzalez@users.noreply.github.com>
2025-08-26 21:28:38 -0500

    Add proper logging (#231)
    * Add proper morgan logging
    
    * Fix log file
    
    * Proper date timestamps
    
    * Add cloudwatch configuration
    
    * Fix cloudwatch agent logging

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -7,9 +7,10 @@ module.exports = {
             wait_ready: true,
             instances: 2,
             listen_timeout: 10 * 1000,
-            out_file: "$HOME/.pm2/logs/api_out.log",
-            err_file: "$HOME/.pm2/logs/api_err.log",
+            out_file: "/home/ubuntu/.pm2/logs/api_out.log",
+            err_file: "/home/ubuntu/.pm2/logs/api_err.log",
             combine_logs: true,
+            log_date_format: "YYYY-MM-DD HH:mm:ss.SSS",
         },
     ],
 };
