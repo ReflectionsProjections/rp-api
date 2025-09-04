a98d60cc6c881f5f36ace25a8b01a6d77546b9e7
nathannwangg <nathannwangg@gmail.com>
2025-09-04 18:43:46 -0500

    Integrate new puzzlebang changes with leaderboard puzzlebang changes.

--- src/services/puzzlebang/puzzlebang-router.ts
+++ src/services/puzzlebang/puzzlebang-router.ts
@@ -1,71 +1,78 @@
 import { Router } from "express";
 import { StatusCodes } from "http-status-codes";
 import { SupabaseDB } from "../../database";
-import RoleChecker from "../../middleware/role-checker";
-import { Role } from "../auth/auth-models";
 import { PuzzlebangCompleteRequestValidator } from "./puzzlebang-validators";
+import PuzzlebangChecker from "../../middleware/puzzlebang-checker";
+import Config from "../../config";
 import { addPoints } from "../attendee/attendee-utils";
 
 const puzzlebangRouter = Router();
 
-puzzlebangRouter.post(
-    "/",
-    RoleChecker([Role.Enum.PUZZLEBANG]),
-    async (req, res) => {
-        const { email, puzzleId } = PuzzlebangCompleteRequestValidator.parse(
-            req.body
-        );
+puzzlebangRouter.use(PuzzlebangChecker);
 
-        // Email is in registration, so we need to get the userId from the email and pull from attendee
-        const { data: registrationData } =
-            await SupabaseDB.REGISTRATIONS.select("userId")
-                .eq("email", email)
-                .maybeSingle()
-                .throwOnError(); // allow 0 rows, which will be gracefully handled
+puzzlebangRouter.post("/", async (req, res) => {
+    const { email, puzzleId } = PuzzlebangCompleteRequestValidator.parse(
+        req.body
+    );
 
-        if (!registrationData) {
-            return res.status(StatusCodes.NOT_FOUND).json({
-                message: "Registration not found.",
-            });
-        }
+    // Email is in registration, so we need to get the userId from the email and pull from attendee
+    const { data: registrationData } = await SupabaseDB.REGISTRATIONS.select(
+        "userId"
+    )
+        .eq("email", email)
+        .maybeSingle()
+        .throwOnError(); // allow 0 rows, which will be gracefully handled
 
-        const userId = registrationData.userId;
-
-        const { data: attendeeData } = await SupabaseDB.ATTENDEES.select(
-            "puzzlesCompleted"
-        )
-            .eq("userId", userId)
-            .maybeSingle()
-            .throwOnError(); // allow 0 rows, which will be gracefully handled
-
-        if (!attendeeData) {
-            return res
-                .status(StatusCodes.NOT_FOUND)
-                .send("Attendee profile not found for this user.");
-        }
-
-        const puzzlesCompleted = attendeeData.puzzlesCompleted ?? [];
-
-        if (puzzlesCompleted.includes(puzzleId)) {
-            return res
-                .status(StatusCodes.CONFLICT)
-                .send("Puzzle already completed.");
-        }
-
-        const updatedPuzzles = [...puzzlesCompleted, puzzleId];
-
-        // Update puzzles completed and add points (2 points per puzzle)
-        await Promise.all([
-            SupabaseDB.ATTENDEES.update({
-                puzzlesCompleted: updatedPuzzles,
-            })
-                .eq("userId", userId)
-                .throwOnError(),
-            addPoints(userId, 2),
-        ]);
-
-        return res.sendStatus(StatusCodes.OK);
+    if (!registrationData) {
+        return res.status(StatusCodes.NOT_FOUND).json({
+            error: "NotFound",
+        });
     }
-);
+
+    const userId = registrationData.userId;
+
+    const { data: attendeeData } = await SupabaseDB.ATTENDEES.select(
+        "puzzlesCompleted"
+    )
+        .eq("userId", userId)
+        .maybeSingle()
+        .throwOnError(); // allow 0 rows, which will be gracefully handled
+
+    if (!attendeeData) {
+        return res.status(StatusCodes.NOT_FOUND).json({
+            error: "NotFound",
+        });
+    }
+
+    const puzzlesCompleted = attendeeData.puzzlesCompleted ?? [];
+
+    if (puzzlesCompleted.includes(puzzleId)) {
+        return res.status(StatusCodes.CONFLICT).json({
+            error: "AlreadyCompleted",
+        });
+    }
+
+    const puzzlePoints = Config.PUZZLEBANG_POINTS.filter(({ idRegex }) =>
+        idRegex.test(puzzleId)
+    ).reduce((prev, curr) => Math.max(prev, curr.points), 0);
+
+    const updatedPuzzles = [...puzzlesCompleted, puzzleId];
+
+    const [{ data: updated }] = await Promise.all([
+        SupabaseDB.ATTENDEES.update({
+            puzzlesCompleted: updatedPuzzles,
+        })
+            .eq("userId", userId)
+            .select("puzzlesCompleted")
+            .single()
+            .throwOnError(),
+        addPoints(userId, puzzlePoints),
+    ]);
+
+    return res.status(StatusCodes.OK).json({
+        email,
+        puzzlesCompleted: updated.puzzlesCompleted,
+    });
+});
 
 export default puzzlebangRouter;
