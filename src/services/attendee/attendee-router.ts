18983e248d93d6357af2ffd9088610197322fc4a
anandani4136 <ra7353@hotmail.com>
2025-09-05 01:14:08 -0500

    Update Colors

--- src/services/attendee/attendee-router.ts
+++ src/services/attendee/attendee-router.ts
@@ -1,13 +1,11 @@
 import { Router } from "express";
 import { StatusCodes } from "http-status-codes";
 import {
-    AttendeeCreateValidator,
     AttendeeIconUpdateValidator,
     AttendeeTagsUpdateValidator,
     EventIdValidator,
 } from "./attendee-validators";
 import { SupabaseDB, IconColorType } from "../../database";
-import { Tiers, IconColors } from "./attendee-schema";
 import RoleChecker from "../../middleware/role-checker";
 import { Role } from "../auth/auth-models";
 import { generateQrHash, getCurrentDay } from "../checkin/checkin-utils";
